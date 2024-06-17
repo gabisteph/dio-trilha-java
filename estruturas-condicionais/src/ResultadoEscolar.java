95220ce5b03e652d09c9f6b7c3b027ab60da9a6e
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-06-17 10:36:55 -0400

    estruturas de condição

---
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,15 @@
+/*
+ * mais de uma linha dentro de um bloco é obrigatório as chaves {}
+ * Condicional Composta
+ */
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7){
+            System.out.println("Aprovado");
+        }else
+            System.out.println("Reprovado");
+    }   
+}
